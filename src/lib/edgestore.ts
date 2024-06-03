e1cf0aa52ce3e174146ff75d48925087755a44f5
Agostinho Sam'l <agostinhosaml832@gmail.com>
2024-06-03 13:25:47 +0100

    sheat

--- src/lib/edgestore.ts
+++
@@ -1,7 +0,0 @@
-import { createEdgeStoreProvider } from '@edgestore/react'
-import type { EdgeStoreRouter } from '~/app/api/edgestore/[...edgestore]/route'
-
-const { EdgeStoreProvider, useEdgeStore } =
-  createEdgeStoreProvider<EdgeStoreRouter>()
-
-export { EdgeStoreProvider, useEdgeStore }
