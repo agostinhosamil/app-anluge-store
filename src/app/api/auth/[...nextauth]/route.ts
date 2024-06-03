fe4bd34afe5d99d2b6c85a329b6561c0361e8de7
Agostinho Sam'l <agostinhosaml832@gmail.com>
2024-06-03 13:21:01 +0100

    sheat

--- src/app/api/auth/[...nextauth]/route.ts
+++
@@ -1,9 +0,0 @@
-import NextAuth from 'next-auth/next'
-
-import { authOptions } from '~/config/auth'
-
-const nextAuthHandler = NextAuth(authOptions)
-
-export const GET = nextAuthHandler
-
-export const POST = nextAuthHandler
