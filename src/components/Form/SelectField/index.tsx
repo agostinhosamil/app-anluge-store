05b8ca2499fa5514f7ff2689666b76852b5b8d47
Agostinho Sam'l <agostinhosaml832@gmail.com>
2024-06-12 15:00:01 +0100

    feat(@components/Form/SelectField): use deep fetch to get the default value/option data

--- src/components/Form/SelectField/index.tsx
+++ src/components/Form/SelectField/index.tsx
@@ -67,7 +67,39 @@ export const SelectField: SelectFieldComponent<OptionProps> = ({
       return undefined
     }
 
-    return data.find(({ value }) => value === defaultValue)
+    const defaultOptionObject = data.find(({ value }) => value === defaultValue)
+
+    if (defaultOptionObject) {
+      return defaultOptionObject
+    }
+
+    const findOptionByValue = (
+      optionValue: string,
+      list: Array<OptionProps>
+    ): OptionProps | undefined => {
+      let i = 0
+
+      for (; i < list.length; i++) {
+        const listItem = list[i]
+
+        if (listItem.value === optionValue) {
+          return listItem
+        }
+
+        if (listItem.options instanceof Array && listItem.options.length >= 1) {
+          const optionFromListItemChildren = findOptionByValue(
+            optionValue,
+            listItem.options
+          )
+
+          if (optionFromListItemChildren) {
+            return optionFromListItemChildren
+          }
+        }
+      }
+    }
+
+    return findOptionByValue(defaultValue, data)
   }
 
   const selectFieldContainerClickHandler: React.MouseEventHandler = event => {
