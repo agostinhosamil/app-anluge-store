5da4c14803e4f542e685b1cbb9740538e0d6d32f
Agostinho Sam'l <agostinhosaml832@gmail.com>
2024-06-13 10:07:47 +0100

    feat(@components/Form/SelectField): customize select field list scroll bar style

--- src/components/Form/SelectField/styles.ts
+++ src/components/Form/SelectField/styles.ts
@@ -129,6 +129,48 @@ export const List = styled.ul<ListProps>`
     props.$maxHeight ? `${props.$maxHeight}px` : 'unset'};
   overflow-y: auto;
   padding: 0px 0px 15px;
+
+  &::-webkit-scrollbar {
+    width: 6px;
+    height: 6px;
+  }
+
+  &::-webkit-scrollbar-button {
+    width: 2px;
+    height: 2px;
+  }
+
+  &::-webkit-scrollbar-thumb {
+    background: #a8a8a8;
+    border: 0px none #ffffff;
+    border-radius: 50px;
+  }
+
+  &::-webkit-scrollbar-thumb:hover {
+    background: #b3b3b3;
+  }
+
+  &::-webkit-scrollbar-thumb:active {
+    background: #9e9e9e;
+  }
+
+  &::-webkit-scrollbar-track {
+    background: #ffffff;
+    border: 0px none #ffffff;
+    border-radius: 50px;
+  }
+
+  &::-webkit-scrollbar-track:hover {
+    background: #ffffff;
+  }
+
+  &::-webkit-scrollbar-track:active {
+    background: #ffffff;
+  }
+
+  &::-webkit-scrollbar-corner {
+    background: transparent;
+  }
 `
 
 export const OptionContainer = styled.div`
