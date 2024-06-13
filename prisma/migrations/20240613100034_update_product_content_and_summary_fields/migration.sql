b0b66785898cd915a99dac4f26aa3646b2101d04
Agostinho Sam'l <agostinhosaml832@gmail.com>
2024-06-13 13:09:04 +0100

    feat(app@database/prisma): update prisma schema

---
+++ prisma/migrations/20240613100034_update_product_content_and_summary_fields/migration.sql
@@ -0,0 +1,3 @@
+-- AlterTable
+ALTER TABLE `product` MODIFY `summary` TINYTEXT NULL,
+    MODIFY `description` LONGTEXT NOT NULL;
---
+++ prisma/migrations/20240613105625_add_order_quantity_field/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE `order` ADD COLUMN `quantity` INTEGER NOT NULL DEFAULT 1;
