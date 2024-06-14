93062c40c8267122c13b306e1f5bfc1d1b549585
Agostinho Sam'l <agostinhosaml832@gmail.com>
2024-06-14 09:17:09 +0100

    feat(@components/RichText): create rich text render component

---
+++ src/components/RichText.tsx
@@ -0,0 +1,32 @@
+import { Fragment } from 'react'
+import { renderRichTextContent } from '~/utils/renderRichTextContent'
+
+export type RichTextProps = {
+  content?: string | Array<string>
+}
+
+export type RichTextComponent = React.FunctionComponent<
+  React.PropsWithChildren & RichTextProps
+>
+
+export const RichText: RichTextComponent = ({ content, ...props }) => {
+  const richTextContent = content instanceof Array ? content : [content]
+  const children =
+    props.children instanceof Array
+      ? [...props.children, ...richTextContent]
+      : [props.children, ...richTextContent]
+
+  return (
+    <Fragment>
+      {children
+        .filter(child => Boolean(child))
+        .map((child, index) => {
+          if (typeof child === 'string') {
+            return renderRichTextContent(child)
+          }
+
+          return <Fragment key={index}>{child}</Fragment>
+        })}
+    </Fragment>
+  )
+}
