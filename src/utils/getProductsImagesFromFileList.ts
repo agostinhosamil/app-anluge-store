c64eedeeda48d7c0f3ea59dd561ecf265cdb9634
Agostinho Sam'l <agostinhosaml832@gmail.com>
2024-06-16 17:20:14 +0100

    feat(@utils/getProductsImagesFromFileList): create util to get product images from a given array of file objects

---
+++ src/utils/getProductsImagesFromFileList.ts
@@ -0,0 +1,69 @@
+import {
+  ProductImagesData,
+  ProductsImagesData
+} from './getProductsImagesFromZipFile'
+import { productImageUploadClient } from './product/imageUploadClient'
+
+const productCodeRegEx = /\s*-?\s*?(\(([a-zA-Z0-9_-]+)\)\s*\.(jpe?g|png|gif))$/
+
+const getProductCodeFromImageName = (imageName: string): string => {
+  const productCodeMatch = productCodeRegEx.exec(imageName)
+
+  if (productCodeMatch) {
+    return String(productCodeMatch[2])
+  }
+
+  return imageName
+}
+
+export type GetProductsImagesFromFileListUtil = (
+  imageFiles: Array<File>
+) => Promise<ProductsImagesData>
+
+export const getProductsImagesFromFileList: GetProductsImagesFromFileListUtil =
+  async imageFiles => {
+    const productsImagesData: Array<null | ProductImagesData> =
+      await Promise.all(
+        imageFiles.map((imageFile): Promise<null | ProductImagesData> => {
+          const imageProductCode = getProductCodeFromImageName(imageFile.name)
+
+          return new Promise(resolve => {
+            const tmpImage = new Image()
+
+            const imageLoadHandler = async () => {
+              const uploadedImage =
+                await productImageUploadClient.uploadFile(imageFile)
+
+              resolve({
+                medias: [
+                  {
+                    fileName: uploadedImage.name
+                  }
+                ],
+                product: {
+                  code: imageProductCode
+                }
+              })
+            }
+
+            try {
+              tmpImage.onload = () => {
+                imageLoadHandler()
+              }
+
+              tmpImage.onerror = () => resolve(null)
+
+              tmpImage.src = URL.createObjectURL(imageFile)
+            } catch (err) {
+              resolve(null)
+            }
+          })
+        })
+      )
+
+    const filteredProductImagesData = productsImagesData.filter(
+      productImagesData => Boolean(productImagesData)
+    )
+
+    return filteredProductImagesData as ProductsImagesData
+  }
