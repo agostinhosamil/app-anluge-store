bb02f8ede588548d1d722e9717ce26d451e7e95a
Agostinho Sam'l <agostinhosaml832@gmail.com>
2024-06-16 13:57:56 +0100

    feat(dashboard@pages/products/image-mass-update): get each image matching a product by code regex and get an array of product objects with code and media data object

---
+++ src/utils/getProductsImagesFromZipFile.ts
@@ -0,0 +1,81 @@
+import { productImageUploadClient } from '@utils/product/imageUploadClient'
+
+import { getZipFileContent } from './getZipFileContent'
+
+const productCodeRegEx = /\s*-?\s*?(\(([a-zA-Z0-9_-]+)\))$/
+
+const getProductCodeFromImageName = (imageName: string): string => {
+  const productCodeMatch = productCodeRegEx.exec(imageName)
+
+  if (productCodeMatch) {
+    return String(productCodeMatch[2])
+  }
+
+  return imageName
+}
+
+export type ProductImagesData = {
+  product: {
+    code: string
+  }
+
+  medias: Array<{
+    fileName: string
+  }>
+}
+
+export type ProductsImagesData = Array<ProductImagesData>
+
+export type GetProductsImagesFromZipFileUtil = (
+  zipFile: File
+) => Promise<ProductsImagesData>
+
+export const getProductsImagesFromZipFile: GetProductsImagesFromZipFileUtil =
+  async zipFile => {
+    const imageFiles = await getZipFileContent(zipFile)
+
+    const productsImagesData: Array<null | ProductImagesData> =
+      await Promise.all(
+        imageFiles.map((imageFile): Promise<null | ProductImagesData> => {
+          const imageProductCode = getProductCodeFromImageName(imageFile.name)
+
+          return new Promise(resolve => {
+            const tmpImage = new Image()
+
+            const imageLoadHandler = async () => {
+              const uploadedImage =
+                await productImageUploadClient.uploadFile(imageFile)
+
+              resolve({
+                medias: [
+                  {
+                    fileName: uploadedImage.name
+                  }
+                ],
+                product: {
+                  code: imageProductCode
+                }
+              })
+            }
+
+            try {
+              tmpImage.onload = () => {
+                imageLoadHandler()
+              }
+
+              tmpImage.onerror = () => resolve(null)
+
+              tmpImage.src = URL.createObjectURL(imageFile)
+            } catch (err) {
+              resolve(null)
+            }
+          })
+        })
+      )
+
+    const filteredProductImagesData = productsImagesData.filter(
+      productImagesData => Boolean(productImagesData)
+    )
+
+    return filteredProductImagesData as ProductsImagesData
+  }
