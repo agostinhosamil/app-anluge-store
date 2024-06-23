22d79b8e5ef19111f19e30e118cc79883bbea31e
Agostinho Sam'l <agostinhosaml832@gmail.com>
2024-06-23 12:55:37 +0100

    feat(store@pages/me/favorites): redirect user to login page if it is unauthenticated

--- src/app/(store)/me/favorites/page.tsx
+++ src/app/(store)/me/favorites/page.tsx
@@ -1,5 +1,18 @@
-"use client";
+import { redirect } from 'next/navigation'
 
-export default function FavoritesCart() {
-  return <div>Favorites here</div>;
+import { auth } from '~/utils/auth'
+import { getCurrentPagePath } from '~/utils/server'
+
+export default async function FavoritesCart() {
+  const userSignInData = await auth()
+
+  if (!userSignInData) {
+    return redirect(`/login?next=${encodeURIComponent(getCurrentPagePath())}`)
+  }
+
+  return (
+    <div>
+      <h1>Favorites here</h1>
+    </div>
+  )
 }
