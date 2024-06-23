d39ca21f2d63a55b752ba20f24fb4f23d80f3c23
Agostinho Sam'l <agostinhosaml832@gmail.com>
2024-06-23 13:56:31 +0100

    feat(store@pages/HomePage): craete home page client component

---
+++ src/app/(store)/HomePage.tsx
@@ -0,0 +1,20 @@
+'use client'
+
+import { NewsFeed } from 'store@components/NewsFeed'
+import { PageContainer } from 'store@components/styles'
+import { CategoryProps } from '~/Types/Category'
+
+type HomePageProps = {
+  categories: Array<CategoryProps>
+}
+
+type HomePageComponent = React.FunctionComponent<HomePageProps>
+
+export const HomePage: HomePageComponent = ({ categories }) => {
+  console.log(categories)
+  return (
+    <PageContainer>
+      <NewsFeed categories={categories} />
+    </PageContainer>
+  )
+}
