7ccf63f7a80b57c1c981b64eb7228e104483bf5f
Agostinho Sam'l <agostinhosaml832@gmail.com>
2024-06-23 13:29:33 +0100

    feat(@utils/generateSlagByTitle): create an util to generate slag by title without including a signature

--- src/utils/generateSlagByTitle.ts
+++ src/utils/generateSlagByTitle.ts
@@ -3,16 +3,22 @@ import { stripStrAccents } from './stripStrAccents'
 export const generateSlagByTitle = (title: string): string => {
   title = stripStrAccents(title)
 
-  const re = /(\s+|[^a-zA-Z0-9_\-\.])/
+  const re = /(\s+|[^a-zA-Z0-9_\-.])/
 
   return title
     .split(re)
     .filter(strSlice => !re.test(strSlice))
     .join('-')
-    .split(/[_\-\.]+/)
+    .split(/[_\-.]+/)
     .join('-')
     .replace(/^(\s*-+\s*)/, '')
     .replace(/(\s*-+\s*)$/, '')
     .toLowerCase()
     .concat(`-${Math.round(Date.now() * Math.random())}`)
+}
+
+export const generateSlagByTitleWithoutSignature = (title: string): string => {
+  const slag = generateSlagByTitle(title)
+
+  return slag.replace(/\s*-\s*([0-9]+)$/, '')
 }
