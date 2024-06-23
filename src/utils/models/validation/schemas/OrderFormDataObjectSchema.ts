410e8a638024e35a813bac8f87417c666a825f2a
Agostinho Sam'l <agostinhosaml832@gmail.com>
2024-06-23 13:38:00 +0100

    feat(@utils/models/validation/schemas/OrderFormDataObjectSchema): update validation rules for the order form

--- src/utils/models/validation/schemas/OrderFormDataObjectSchema.ts
+++ src/utils/models/validation/schemas/OrderFormDataObjectSchema.ts
@@ -3,8 +3,16 @@ import { z } from 'zod'
 export const OrderFormDataObjectSchema = z.object({
   user: z
     .object({
-      name: z.string().min(5).max(80),
-      email: z.string().min(5).max(80).email(),
+      id: z.string().nullable().optional(),
+      name: z
+        .string()
+        .min(5, 'O nome não pode ser tão curto')
+        .max(80, 'O nome não pode ser tão longo'),
+      email: z
+        .string()
+        .min(5)
+        .max(80)
+        .email('Precisa fornecer um endereço de email válido'),
       phone: z
         .string()
         .min(5)
@@ -20,11 +28,17 @@ export const OrderFormDataObjectSchema = z.object({
         .refine(phone => {
           const phoneRe = /^(((\+?244)?9[0-9]{8}))$/
           return phoneRe.test(phone)
-        }),
+        }, 'Precisa fornecer um número de telefone angolano válido'),
       password: z.string().min(8).max(80),
-      passwordConfirmation: z.string()
+      passwordConfirmation: z.string().optional()
     })
-    .refine(user => user.password === user.passwordConfirmation),
+    .refine(
+      user =>
+        typeof user.passwordConfirmation === 'undefined'
+          ? true
+          : user.password === user.passwordConfirmation,
+      'As palavras passes não são iguais'
+    ),
   orders: z
     .array(
       z.object({
@@ -39,3 +53,5 @@ export const OrderFormDataObjectSchema = z.object({
     )
     .min(1)
 })
+
+export type OrderFormDataObject = z.infer<typeof OrderFormDataObjectSchema>
