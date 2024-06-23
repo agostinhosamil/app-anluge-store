48f3465d3643541a977fe23891acdcbebb7aa37c
Agostinho Sam'l <agostinhosaml832@gmail.com>
2024-06-23 09:52:46 +0100

    style(eslint): add eslint typescript rules

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,20 +1,29 @@
-import pluginJs from "@eslint/js";
-import pluginReactConfig from "eslint-plugin-react/configs/recommended.js";
-import globals from "globals";
-import tseslint from "typescript-eslint";
+import pluginJs from '@eslint/js'
+import pluginReactConfig from 'eslint-plugin-react/configs/recommended.js'
+import globals from 'globals'
+import tseslint from 'typescript-eslint'
 
 const eslintConfig = [
   {
     languageOptions: {
       globals: {
         ...globals.browser,
-        ...globals.node,
-      },
-    },
+        ...globals.node
+      }
+    }
   },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
   pluginReactConfig,
-];
+  {
+    rules: {
+      'react/react-in-jsx-scope': 'off',
+      'react/jsx-uses-react': 'off',
+      '@typescript-eslint/no-explicit-any': 'off',
+      'react/prop-types': 'off',
+      'no-empty': 'off'
+    }
+  }
+]
 
-export default eslintConfig;
+export default eslintConfig
