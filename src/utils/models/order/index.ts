7194e7653843443778478e7a0f3d07b7eb88644d
Agostinho Sam'l <agostinhosaml832@gmail.com>
2024-06-23 13:35:19 +0100

    feat(@utils/models/order): [types] create failure and success response object types

--- src/utils/models/order/index.ts
+++ src/utils/models/order/index.ts
@@ -1,33 +1,74 @@
 import { z } from 'zod'
 
-import { OrderFormDataObjectSchema } from '../validation/schemas/OrderFormDataObjectSchema'
-import { orderLinkFactory } from './orderLinkFactory'
-import { orderMessageFactory } from './orderMessageFactory'
+import { axios } from '@services/axios'
+import { OrderFormDataObjectSchema } from '@utils/models/validation/schemas/OrderFormDataObjectSchema'
+import { CartProps } from '~/Types/Cart'
 
 export type OrderFormDataObject = z.infer<typeof OrderFormDataObjectSchema>
 
-export const createOrder = async (orderFormDataObject: OrderFormDataObject) => {
+type CreateOrderRequestSuccessResponse = {
+  success: true
+  cart: CartProps
+}
+
+type CreateOrderRequestFailureResponse = {
+  success: false
+  cart: null
+}
+
+export type CreateOrderRequestResponse =
+  | CreateOrderRequestSuccessResponse
+  | CreateOrderRequestFailureResponse
+
+export const createOrder = async (
+  orderFormDataObject: OrderFormDataObject
+): Promise<CreateOrderRequestResponse> => {
   const validFormData = OrderFormDataObjectSchema.safeParse(orderFormDataObject)
 
   if (!validFormData.success) {
-    console.log('Error')
-    return
+    console.log(
+      '>> orderFormDataObject Error: ',
+      orderFormDataObject,
+      '\n\nErrors: ',
+      'formErrors: ',
+      validFormData.error.formErrors,
+      'errors: ',
+      validFormData.error.errors
+    )
+
+    return {
+      cart: null,
+      success: false
+    }
   }
 
-  const { orders, user } = validFormData.data
-
-  const orderMessage = orderMessageFactory({
-    orders,
-    user
-  })
-
-  const orderLink = orderLinkFactory(orderMessage)
+  // const { orders, user } = validFormData.data
+  // const orderMessage = orderMessageFactory({
+  //   orders,
+  //   user
+  // })
+  // const orderLink = orderLinkFactory(orderMessage)
   // console.log({
   //   orderMessage,
   //   decodedOrderMessage: decodeURIComponent(orderMessage)
   // })
+  // window.open(orderLink, '_blank')
 
-  window.open(orderLink, '_blank')
+  try {
+    const response = await axios.post<CreateOrderRequestResponse>(
+      '/store/cart',
+      validFormData.data
+    )
 
-  return true
+    if (typeof response.data === 'object' && response.data.success) {
+      return response.data
+    }
+  } catch (err) {
+    // pass
+  }
+
+  return {
+    cart: null,
+    success: false
+  }
 }
