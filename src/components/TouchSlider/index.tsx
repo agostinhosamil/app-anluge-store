f2960ccc17ce0851df1ab1c3aa52134bc653d36e
Agostinho Sam'l <agostinhosaml832@gmail.com>
2024-06-23 16:50:30 +0100

    fix(@components/TouchSlider): correct slider swipe effect

--- src/components/TouchSlider/index.tsx
+++ src/components/TouchSlider/index.tsx
@@ -26,39 +26,29 @@ type TouchSliderComponent = React.FunctionComponent<
 export const TouchSlider: TouchSliderComponent = props => {
   const [index, setIndex] = useState<number>(0)
   const [sliderX, setSliderX] = useState<number>(0)
-  // const [grabbing, setGrabbing] = useState<boolean>(false)
 
   const wrapperRef = useRef<HTMLDivElement>(null)
   const containerRef = useRef<HTMLDivElement>(null)
-  // const mouseHoldingWrapperState = useRef<boolean>(false)
   const grabbingState = useRef<boolean>(false)
   const mouseXValuesState = useRef<MouseXValues>({
     old: -1,
     current: -1
   })
 
-  const isLastSlideActive = () => {
-    const sliderWrapperElement = wrapperRef.current
-
-    if (!sliderWrapperElement) {
-      return false
-    }
-
-    console.log(
-      `>>> index >= sliderWrapperElement.children.length: ${index} >= ${sliderWrapperElement.children.length}`
-    )
-
-    return index >= sliderWrapperElement.children.length
-  }
-
   const resolveMouseDirection = (): MouseDirection | null => {
     const { current, old } = mouseXValuesState.current
 
-    if (current === old) {
-      return null
+    const MIN_SWIPE_SIZE = 70
+
+    if (current > old && current - old >= MIN_SWIPE_SIZE) {
+      return MouseDirection.RIGHT
     }
 
-    return current >= old ? MouseDirection.RIGHT : MouseDirection.LEFT
+    if (current < old && old - current >= MIN_SWIPE_SIZE) {
+      return MouseDirection.LEFT
+    }
+
+    return null
   }
 
   const resolveSliderVelocity = (): number => {
@@ -67,21 +57,6 @@ export const TouchSlider: TouchSliderComponent = props => {
     if (!sliderWrapperElement) {
       return 200
     }
-
-    // const currentElementIndex = sliderWrapperElement.children.length / sliderX
-    // const nextElementIndex =
-    //   sliderDirection === MouseDirection.LEFT
-    //     ? currentElementIndex - 1
-    //     : currentElementIndex + 1
-
-    // // console.log('>>> nextElementIndex: ', nextElementIndex)
-
-    // return 320
-
-    // console.log(
-    //   '>>> slider velocity: ',
-    //   sliderWrapperElement.offsetWidth / sliderWrapperElement.children.length
-    // )
 
     const sliderWrapperElementFirstChild = sliderWrapperElement
       .children[0] as HTMLElement
@@ -92,15 +67,12 @@ export const TouchSlider: TouchSliderComponent = props => {
   }
 
   const sliderWrapperMouseDownHandler: React.MouseEventHandler = event => {
-    // console.log('>>> sliderWrapperMouseDownHandler')
-    // setTimeout(() => {
     grabbingState.current = true
 
     mouseXValuesState.current = {
       ...mouseXValuesState.current,
       old: event.clientX
     }
-    // }, 100)
   }
 
   const sliderWrapperMouseUpHandler: React.MouseEventHandler = event => {
@@ -111,21 +83,46 @@ export const TouchSlider: TouchSliderComponent = props => {
       current: event.clientX
     }
 
+    let velocity = resolveSliderVelocity()
     const direction = resolveMouseDirection()
 
     if (!direction) {
       return
     }
 
-    const velocity = resolveSliderVelocity()
+    if (
+      !(
+        wrapperRef.current &&
+        wrapperRef.current.children.length >= 1 &&
+        containerRef.current
+      )
+    ) {
+      return
+    }
+
+    const wrapperElement = wrapperRef.current
+    const containerElement = containerRef.current
+
+    const wrapperElementCoordinates = wrapperElement.getBoundingClientRect()
+    const containerElementCoordinates = containerElement.getBoundingClientRect()
+
+    const wrapperElementXw =
+      wrapperElementCoordinates.x - velocity + wrapperElementCoordinates.width
+    const containerElementXw =
+      containerElementCoordinates.x + containerElementCoordinates.width
 
     switch (direction) {
       case MouseDirection.LEFT:
-        if (!isLastSlideActive()) {
-          setSliderX(sliderX - velocity)
+        if (containerElementXw >= wrapperElementXw) {
+          const containerElementXwDiffToWrapperIs =
+            containerElementXw - wrapperElementXw
 
-          setIndex(index + 1)
+          velocity -= containerElementXwDiffToWrapperIs
         }
+
+        setSliderX(sliderX - velocity)
+
+        setIndex(index + 1)
         break
 
       case MouseDirection.RIGHT:
@@ -145,67 +142,6 @@ export const TouchSlider: TouchSliderComponent = props => {
   }
 
   const sliderWrapperMouseMoveHandler = () => {}
-
-  useEffect(() => {
-    if (
-      !(
-        wrapperRef.current &&
-        wrapperRef.current.children.length >= 1 &&
-        containerRef.current
-      )
-    ) {
-      return
-    }
-
-    // const containerElement = containerRef.current
-    // const wrapperElementChildren = wrapperRef.current.children
-    // const wrapperElementLastChild = wrapperElementChildren[-1 + wrapperElementChildren.length] as HTMLElement
-
-    // const containerElementCoordinates = containerElement.getBoundingClientRect()
-    // const wrapperElementLastChildCoordinates =
-    //   wrapperElementLastChild.getBoundingClientRect()
-
-    // ...
-
-    // const wrapperElement = wrapperRef.current
-    // const wrapperLastChild =
-    //   wrapperRef.current.children[-1 + wrapperRef.current.children.length]
-    // const wrapperElementCoordinates = getElementCoordinates(wrapperElement)
-    // const containerElementCoordinates = getElementCoordinates(
-    //   containerRef.current
-    // )
-    // const wrapperElementXRelativeToContainer =
-    //   wrapperElementCoordinates.x - containerElementCoordinates.x
-    // const wrapperElementXwRelativeToContainer =
-    //   wrapperElementXRelativeToContainer + containerRef.current.offsetWidth
-    // const x = containerElementCoordinates.x - wrapperElementCoordinates.x
-    // const xw = x + containerRef.current.offsetWidth
-    // console.log('>>> X: ', x)
-    // const wrapperElementXw =
-    //   wrapperElementCoordinates.xw - containerRef.current.offsetWidth
-    // if (
-    //   wrapperElementCoordinates.x - x + containerRef.current.offsetWidth <=
-    //   containerElementCoordinates.xw
-    // ) {
-    //   console.log('>>> out of range')
-    // }
-    // console.log(
-    //   '>>> wrapperElementXwRelativeToContainer: ',
-    //   wrapperElementXwRelativeToContainer
-    // )
-    // if (wrapperElementXwRelativeToContainer < 0) {
-    //   console.log('>>> Limit reached!!!')
-    // }
-    // if (wrapperLastChildCoordinates.xw < containerElementCoordinates.xw) {
-    //   const wrapperToContainerXDiff =
-    //     containerElementCoordinates.xw - wrapperLastChildCoordinates.xw
-    //   console.log(`
-    //     >>> wrapperLastChildCoordinates.xw < containerElementCoordinates.xw => ${wrapperLastChildCoordinates.xw} < ${containerElementCoordinates.xw}
-    //     >>> wrapperToContainerXDiff: ${wrapperToContainerXDiff}
-    //   `)
-    //   setSliderX(sliderX + wrapperToContainerXDiff)
-    // }
-  }, [sliderX])
 
   useEffect(() => {
     if (sliderX > 0) {
