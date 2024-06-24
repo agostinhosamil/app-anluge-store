9b232839aa291753ba56e6cdd58d8601d2994586
Agostinho Sam'l <agostinhosaml832@gmail.com>
2024-06-24 11:48:27 +0100

    fix(prisma/migrations): correct passowrd table name setting it to lowercase

--- prisma/migrations/20240620093256_create_password_model_and_set_user_default_image/migration.sql
+++ prisma/migrations/20240620093256_create_password_model_and_set_user_default_image/migration.sql
@@ -2,7 +2,7 @@
 ALTER TABLE `user` MODIFY `image` VARCHAR(191) NULL DEFAULT 'user-avatar-placeholder.jpg';
 
 -- CreateTable
-CREATE TABLE `Password` (
+CREATE TABLE `password` (
     `id` INTEGER NOT NULL AUTO_INCREMENT,
     `content` VARCHAR(191) NOT NULL,
     `userId` VARCHAR(191) NOT NULL,
@@ -11,4 +11,4 @@ CREATE TABLE `Password` (
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
 -- AddForeignKey
-ALTER TABLE `Password` ADD CONSTRAINT `Password_userId_fkey` FOREIGN KEY (`userId`) REFERENCES `user`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE `password` ADD CONSTRAINT `password_userId_fkey` FOREIGN KEY (`userId`) REFERENCES `user`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
