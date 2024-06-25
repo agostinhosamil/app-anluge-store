2a35ffd0f8ff4a105bad01a93b59ee2c6fd9c4b3
Agostinho Sam'l <agostinhosaml832@gmail.com>
2024-06-25 14:58:14 +0100

    feat(store@components/Header/HeaderSlider): [SliderImageWrapper.Image] resize image on different screen sizes

--- src/components/store/Header/HeaderSlider.tsx
+++ src/components/store/Header/HeaderSlider.tsx
@@ -1,10 +1,33 @@
 import Image from 'next/image'
+import { useEffect, useState } from 'react'
 import Carousel from 'react-bootstrap/Carousel'
 
 import { SliderImageWrapper } from './styles'
 
 export const HeaderSlider = () => {
+  const [imageHeight, setImageHeight] = useState<number>(340)
+
   const sliderImages = ['/image001.png', '/image002.png', '/image003.png']
+
+  useEffect(() => {
+    const windowScrollHandler = () => {
+      if (window.innerWidth <= 565 && imageHeight > 220) {
+        setImageHeight(220)
+      }
+
+      if (window.innerWidth > 565 && imageHeight <= 220) {
+        setImageHeight(340)
+      }
+    }
+
+    window.addEventListener('resize', windowScrollHandler)
+
+    windowScrollHandler()
+
+    return () => {
+      window.removeEventListener('resize', windowScrollHandler)
+    }
+  }, [imageHeight])
 
   return (
     <div>
@@ -12,7 +35,7 @@ export const HeaderSlider = () => {
         {sliderImages.map((image, index) => (
           <Carousel.Item key={index}>
             <SliderImageWrapper>
-              <Image src={image} alt={image} width={600} height={340} />
+              <Image src={image} alt={image} width={600} height={imageHeight} />
             </SliderImageWrapper>
           </Carousel.Item>
         ))}
