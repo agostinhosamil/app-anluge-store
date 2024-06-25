f7b5f6404223e431aefa99e0c295d8d7040739d9
Agostinho Sam'l <agostinhosaml832@gmail.com>
2024-06-25 11:29:52 +0100

    feat(sotre@pages/me/cart/orders/[cartId]): [cancel] terminate cancel order page

--- src/app/(store)/me/orders/[cartId]/cancel/content.tsx
+++ src/app/(store)/me/orders/[cartId]/cancel/content.tsx
@@ -1,8 +1,39 @@
 'use client'
 
+import Col from 'react-bootstrap/Col'
+import Row from 'react-bootstrap/Row'
+
 import { CartProps } from 'Types/Cart'
-import { Container } from '~/components/styled'
-import { PageWrapper } from './styles'
+import { Container, LoadingScreen } from '~/components/styled'
+import { Slide, TouchSlider } from '~/components/TouchSlider'
+import { resolveProductImageUrl } from '~/utils'
+
+import { useState } from 'react'
+import { Spinner } from 'react-bootstrap'
+import { BiSolidOffer } from 'react-icons/bi'
+import {
+  FaAngleLeft,
+  FaArrowRightArrowLeft,
+  FaPhone,
+  FaTeamspeak
+} from 'react-icons/fa6'
+import { cancelOrder } from '~/utils/models/order'
+import { redirect } from '~/utils/navigation'
+import { reasons } from './reasons'
+import {
+  CancelLink,
+  CheckOptionsGroup,
+  CheckOptionsList,
+  OptionalLink,
+  OptionalLinksListWrapper,
+  OrderItem,
+  OrderItemsSliderWrapper,
+  PageWrapper,
+  Paragraph,
+  SubmitButton,
+  SubTitle,
+  Title
+} from './styles'
 
 type ContentProps = {
   cart: CartProps
@@ -11,10 +42,178 @@ type ContentProps = {
 type ContentComponent = React.FunctionComponent<ContentProps>
 
 export const Content: ContentComponent = props => {
+  const [loading, setLoading] = useState<boolean>(false)
+
+  const formSubmitHandler: React.FormEventHandler = async event => {
+    event.preventDefault()
+
+    setLoading(true)
+
+    const orderCanceled = await cancelOrder(props.cart.id)
+
+    if (orderCanceled) {
+      return redirect('/me/orders')
+    }
+
+    // TODO: handle this using custom alert/message box
+    alert(
+      'Perdão, algo correu mal ao processar o seu pedido... Tente novamente mais tarde'
+    )
+
+    setLoading(false)
+  }
+
   return (
     <Container>
+      {loading && (
+        <LoadingScreen>
+          <i>
+            <Spinner />
+          </i>
+          <span>A cancelar o pedido...</span>
+        </LoadingScreen>
+      )}
       <PageWrapper>
-        <h1>Cancel cart: {props.cart.code}</h1>
+        <form
+          method="post"
+          action={`/api/store/orders/${props.cart.id}/cancel`}
+          onSubmit={event => formSubmitHandler(event)}
+        >
+          <Title>Que pena; estavas tão perto...!</Title>
+          <Paragraph>
+            Entendemos que às vezes surgem dúvidas ou imprevistos que podem
+            levar você a reconsiderar sua compra. Queremos assegurar que estamos
+            aqui para ajudar e tornar sua experiência conosco o mais tranquila
+            possível. Se surgiram novas informações ou você simplesmente mudou
+            de ideia, estamos prontos para oferecer assistência imediata. Nosso
+            objetivo é garantir que cada cliente se sinta totalmente satisfeito
+            com sua decisão de compra, e estamos comprometidos em resolver
+            qualquer questão que possa ter surgido durante o processo de compra.
+          </Paragraph>
+          <CheckOptionsGroup>
+            <strong>
+              Por favor, ajude-nos a melhorar o que quer que esteja a faltar na
+              nossa plataforma.
+              <br />O que o levou a cancelar este pedido?
+            </strong>
+            <CheckOptionsList>
+              {reasons.map((reason, i) => (
+                <li key={i}>
+                  <input
+                    type="radio"
+                    name="cancel[reason]"
+                    value={i}
+                    id={`reason${i}`}
+                  />
+                  <label htmlFor={`reason${i}`}>
+                    <i />
+                    <span>{reason.title}</span>
+                    <p suppressHydrationWarning>{reason.description}</p>
+                  </label>
+                </li>
+              ))}
+            </CheckOptionsList>
+          </CheckOptionsGroup>
+          <SubTitle>O que está a deixar para trás...</SubTitle>
+          <OrderItemsSliderWrapper>
+            <TouchSlider showButtons showIndicators>
+              {props.cart.orders.map(order => (
+                <Slide key={order.id}>
+                  <OrderItem>
+                    <div
+                      style={{
+                        backgroundImage: `url(${resolveProductImageUrl(order.product)})`
+                      }}
+                    />
+                    <strong>{order.product.name}</strong>
+                    <span>{order.quantity}</span>
+                  </OrderItem>
+                </Slide>
+              ))}
+            </TouchSlider>
+          </OrderItemsSliderWrapper>
+          <SubTitle>
+            Se tiver a ver com algum problema ligado a plataforma ou aos nossos
+            serviços, talvez possamos ajudar:
+          </SubTitle>
+          <OptionalLinksListWrapper>
+            <Row>
+              <Col md={4}>
+                <OptionalLink href={''}>
+                  <i>
+                    <FaPhone />
+                  </i>
+                  <data>
+                    <strong>Contactar apoio a cliente</strong>
+                    <span>
+                      Tire dúvidas sobre como usar a plataforma da melhor forma
+                      possível.
+                    </span>
+                  </data>
+                </OptionalLink>
+              </Col>
+              <Col md={8}>
+                <OptionalLink href={''} $color="green">
+                  <i>
+                    <FaTeamspeak />
+                  </i>
+                  <data>
+                    <strong>Negociar com a nossa equipe de vendas</strong>
+                    <span>
+                      Negocie possíveis descontos com a nossa equipe de vendas e
+                      veja o que ainda podem fazer por si.
+                    </span>
+                  </data>
+                </OptionalLink>
+              </Col>
+            </Row>
+            <Row className="mt-4">
+              <Col md={7}>
+                <OptionalLink href={''} $color="violet">
+                  <i>
+                    <BiSolidOffer />
+                  </i>
+                  <data>
+                    <strong>Ofertas Exclusivas</strong>
+                    <span>
+                      Como forma de agradecimento pela sua preferência,
+                      oferecemos ofertas exclusivas e descontos para que você
+                      possa reconsiderar sua decisão.
+                    </span>
+                  </data>
+                </OptionalLink>
+              </Col>
+              <Col md={5}>
+                <OptionalLink href={''} $color="yellow">
+                  <i>
+                    <FaArrowRightArrowLeft />
+                  </i>
+                  <data>
+                    <strong>Fazer troca de produtos</strong>
+                    <span>
+                      Se não tem certeza sobre o produto escolhido, podemos
+                      ajudar a encontrar uma alternativa que atenda melhor às
+                      suas necessidades.
+                    </span>
+                  </data>
+                </OptionalLink>
+              </Col>
+            </Row>
+          </OptionalLinksListWrapper>
+          <Paragraph style={{ marginTop: '0px' }}>
+            Não hesite em entrar em contato conosco através do nosso chat ao
+            vivo ou pelo número [Inserir número de contato] para que possamos
+            resolver qualquer dúvida ou preocupação que você tenha. Estamos aqui
+            para tornar sua experiência de compra conosco excepcional.
+          </Paragraph>
+          <SubmitButton type="submit">Cancelar pedido</SubmitButton>
+          <CancelLink href="/me/orders">
+            <i>
+              <FaAngleLeft />
+            </i>
+            <span>Voltar</span>
+          </CancelLink>
+        </form>
       </PageWrapper>
     </Container>
   )
--- src/app/(store)/me/orders/[cartId]/cancel/styles.ts
+++ src/app/(store)/me/orders/[cartId]/cancel/styles.ts
@@ -1,5 +1,299 @@
-import styled from 'styled-components'
+import Link from 'next/link'
+import styled, { css } from 'styled-components'
 
 export const PageWrapper = styled.div`
   width: 100%;
+  max-width: 950px;
+  height: auto;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 120px 40px 20px;
+  text-align: center;
+  margin: auto;
+
+  form {
+    width: 100%;
+    height: auto;
+    text-align: center;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
+`
+
+export const Title = styled.h1`
+  font-size: 57px;
+  font-weight: 900;
+  color: #882828;
+  max-width: 80%;
+  margin: auto;
+  display: block;
+`
+
+export const SubTitle = styled.h2`
+  font-size: 43px;
+  font-weight: 700;
+  color: #272727;
+`
+
+export const Paragraph = styled.p`
+  width: 82%;
+  display: block;
+  margin: 45px auto;
+  font-size: 24px;
+  color: #888888;
+  font-weight: 300;
+`
+
+export const CheckOptionsGroup = styled.div`
+  width: 100%;
+  height: auto;
+  background-color: #eeeeee;
+  /* box-shadow: 0px 0px 17px 2px rgb(0 0 0 / 10%); */
+  border-radius: 16px;
+  -webkit-border-radius: 16px;
+  margin: 20px auto 40px;
+  padding: 75px 45px;
+
+  strong {
+    display: block;
+    font-size: 25px;
+    font-weight: 700;
+    color: #606060;
+    padding: 40px 10%;
+    line-height: 30px;
+  }
+`
+
+export const CheckOptionsList = styled.ol`
+  width: 100%;
+  padding: 45px 40px 0px;
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  gap: 20px;
+
+  li {
+    width: calc(100% / 3 - 20px);
+    height: auto;
+    display: block;
+    background-color: #ffffff;
+    border-radius: 8px;
+    -webkit-border-radius: 8px;
+    box-shadow: 0px 0px 5px 1px rgba(0 0 0 / 13%);
+
+    input[type='radio']:checked ~ label {
+      background-color: rgb(140 11 11 / 35%);
+
+      * {
+        color: #ffffff;
+      }
+    }
+
+    input[type='radio'] {
+      display: none;
+    }
+
+    label {
+      display: block;
+      width: 100%;
+      height: 100%;
+      padding: 35px;
+      position: relative;
+      border-radius: inherit;
+      -webkit-border-radius: inherit;
+      cursor: pointer;
+      user-select: none;
+      -webkit-user-select: none;
+
+      * {
+        user-select: none;
+        -webkit-user-select: none;
+      }
+
+      span {
+        font-weight: 900;
+        color: #444444;
+        font-size: 18px;
+        line-height: 17px;
+      }
+
+      p {
+        font-weight: 300;
+        margin-top: 12px;
+        font-size: 12px;
+      }
+    }
+  }
+`
+
+export const OrderItemsSliderWrapper = styled.div`
+  width: 100%;
+  height: auto;
+  display: block;
+  padding: 45px 20px;
+`
+
+export const OrderItem = styled.div`
+  width: 190px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  padding: 0px;
+  border: 0px;
+  position: relative;
+  gap: 7px;
+
+  div {
+    width: 100%;
+    height: 280px;
+    background-color: #e0e0e0;
+    border-radius: 12px;
+    -webkit-border-radius: 12px;
+    background-position: center;
+    background-size: cover;
+    background-repeat: no-repeat;
+    background-attachment: scroll;
+    box-shadow: rgb(0 0 0 / 28%) 2px 2px 12px 3px;
+  }
+
+  strong {
+    font-size: 14px;
+    font-weight: 300;
+    word-break: break-word;
+    white-space: break-spaces;
+    display: -webkit-box;
+    width: 100%;
+    padding: 15px 0px 9px;
+    overflow-y: hidden;
+    -webkit-box-orient: vertical;
+    -webkit-line-clamp: 4;
+    color: #444444;
+    height: 100px;
+  }
+
+  span {
+    display: inline-block;
+    font-weight: 900;
+    color: #b0b0b0;
+    font-size: 21px;
+  }
+`
+
+export const OptionalLinksListWrapper = styled.div`
+  width: 100%;
+  padding: 60px 0px;
+  text-align: left;
+`
+
+const optionalLinkColors = {
+  blue: css`
+    background-color: #e4f8fa;
+    border-color: #b2ebf2;
+    color: #153438;
+  `,
+
+  green: css`
+    background-color: #f4fae3;
+    border-color: #e1f2b2;
+    color: #869759;
+  `,
+
+  violet: css`
+    background-color: rgb(246 83 243 / 9%);
+    border-color: #f653f3;
+    color: #a933a7;
+  `,
+
+  yellow: css`
+    background-color: rgb(247 247 1 / 9%);
+    border-color: #f7f701;
+    color: #b0b009;
+  `
+}
+
+type OptionalLinkProps = {
+  $color?: keyof typeof optionalLinkColors
+}
+
+export const OptionalLink = styled(Link)<OptionalLinkProps>`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  padding: 32px;
+  border-style: solid;
+  border-width: 1px;
+  border-radius: 8px;
+  -webkit-border-radius: 8px;
+  transition: all 0.3s ease-in-out;
+  ${props => optionalLinkColors[props.$color || 'blue']}
+
+  &:hover {
+    transform: scale(1.04185);
+  }
+
+  * {
+    color: inherit;
+  }
+
+  i {
+    display: inline-flex;
+    font-size: 35px;
+    margin-right: 22px;
+  }
+
+  data {
+    width: 100%;
+    display: inline-flex;
+    flex-direction: column;
+    gap: 8px;
+
+    strong {
+      font-weight: 800;
+      font-size: 16px;
+    }
+
+    span {
+      font-size: 12px;
+    }
+  }
+`
+
+export const SubmitButton = styled.button`
+  background-color: rgb(255 0 0 / 10%);
+  color: #840e0e;
+  border: 1px solid #840e0e;
+  border-radius: 120px;
+  -webkit-border-radius: 120px;
+  padding: 21px 85px;
+  outline: 0px;
+
+  &:hover {
+    background-color: rgb(255 0 0 / 25%);
+  }
+
+  &:active {
+    background-color: rgb(255 0 0 / 35%);
+  }
+`
+
+export const CancelLink = styled(Link)`
+  margin: 35px auto 0px;
+  color: #333333;
+  font-weight: 400;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+
+  i {
+    display: inline-flex;
+    margin-right: 7px;
+  }
+
+  &:hover {
+    text-decoration: underline;
+  }
 `
