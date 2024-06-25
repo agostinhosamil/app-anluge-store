e68b19a193d171380e23f0bcd1eca1afedb1f182
Agostinho Sam'l <agostinhosaml832@gmail.com>
2024-06-25 13:22:40 +0100

    feat(@routes/auth): [signout] use signOut util to remove stored session user data

--- src/app/api/auth/logout.ts
+++ src/app/api/auth/logout.ts
@@ -1,12 +1,9 @@
-import { cookies } from 'next/headers'
 import { redirect } from 'next/navigation'
 
-const logoutHandler = () => {
-  cookies().set({
-    name: String(process.env.APP_AUTH_COOKIE_NAME),
-    value: 'null',
-    path: '/'
-  })
+import { signOut } from '~/utils/auth'
+
+const logoutHandler = async () => {
+  await signOut()
 
   redirect('/')
 }
