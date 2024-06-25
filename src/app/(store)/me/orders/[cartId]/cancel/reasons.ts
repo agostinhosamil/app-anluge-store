a72760eaa03722226bc548461109cff3c9e8b638
Agostinho Sam'l <agostinhosaml832@gmail.com>
2024-06-25 09:22:04 +0100

    feat(store@pages/me/orders/[cartId]/cancel): [reasons] create static register for possible user order cancel

---
+++ src/app/(store)/me/orders/[cartId]/cancel/reasons.ts
@@ -0,0 +1,47 @@
+export const reasons = [
+  {
+    title: 'Mudança de mente ou arrependimento',
+    description:
+      'Decidi que simplesmente não quero mais o produto após fazer o uma análise.'
+  },
+  {
+    title: 'Achei outra loja a vender a um preço mais baixo',
+    description:
+      'Descobri que o mesmo produto está disponível por um preço mais baixo em outro lugar.'
+  },
+  {
+    title: 'Prazo de entrega muito longo',
+    description:
+      'Percebi que o tempo de entrega é muito longo e preciso do produto mais rapidamente'
+  },
+  {
+    title: 'Problemas com o pagamento',
+    description:
+      'Dificuldades técnicas ou erros ocorreram durante o processo de pagamento, por isso estou a cancelar o pedido.'
+  },
+  {
+    title: 'Informações do produto incorretas',
+    description:
+      'Percebi que as informações sobre o produto são enganosas ou incorretas, por isso decidi cancelar o pedido.'
+  },
+  {
+    title: 'Má experiência',
+    description:
+      'Questões como atendimento ao cliente ruim, falta de comunicação ou dificuldade em resolver problemas levaram-me a cancelar o pedido.'
+  },
+  {
+    title: 'Problemas de estoque',
+    description:
+      'O produto está fora de estoque ou indisponível após a compra, por isso optei por cancelar o pedido.'
+  },
+  {
+    title: 'Condições de devolução e políticas',
+    description:
+      'Não concordar com as condições de devolução ou políticas da loja, me levaram a cancelar o pedido antes que o produto seja enviado.'
+  },
+  {
+    title: 'Problemas de segurança',
+    description:
+      'Percebi questões de segurança, como preocupações com a proteção de dados pessoais durante o processo de compra, então optei por cancelar o pedido por precaução com os meus dados pessoais.'
+  }
+]
