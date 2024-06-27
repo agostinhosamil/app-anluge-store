a968c031a7e4f4711de36ed494ad3952a59ec0da
Agostinho Sam'l <agostinhosaml832@gmail.com>
2024-06-27 05:37:15 +0100

    feat(app@models/User): create user favorites CRUD [CREATE]

--- src/app/api/users/favorites/route.ts
+++ src/app/api/users/favorites/route.ts
@@ -1,10 +1,12 @@
 import { NextResponse } from 'next/server'
+import { z } from 'zod'
 
 import { prisma } from '~/services/prisma'
 import { NextApiHandler } from '~/Types/next'
 import { ProductProps } from '~/Types/Product'
 import { productIncludeFactory } from '~/utils/product'
 import { redirectToLoginIfUnAuthenticated } from '~/utils/server'
+import { getRequestBody } from '~/utils/server/getRequestBody'
 
 export const GET: NextApiHandler = async () => {
   const signInData = await redirectToLoginIfUnAuthenticated()
@@ -34,6 +36,91 @@ export const GET: NextApiHandler = async () => {
   return NextResponse.json(userFavorites, {
     headers: {
       'x-favorites-count': String(userFavorites.length)
+    }
+  })
+}
+
+const PostRequestBodySchema = z.object({
+  products: z
+    .array(
+      z.object({
+        id: z.string().min(5).cuid()
+      })
+    )
+    .min(1)
+    .refine(async productsData => {
+      const products = await prisma.$transaction(
+        productsData.map(productData => {
+          return prisma.product.findUnique({
+            where: {
+              id: productData.id
+            },
+
+            select: {
+              id: true
+            }
+          })
+        })
+      )
+
+      return !products.some(product => !product)
+    })
+})
+
+type PostRequestBodyProps = z.infer<typeof PostRequestBodySchema>
+
+export const POST: NextApiHandler = async request => {
+  const signInData = await redirectToLoginIfUnAuthenticated()
+
+  const { user } = signInData
+
+  const clientRequestBody = await getRequestBody<PostRequestBodyProps>(request)
+
+  const requestBody =
+    await PostRequestBodySchema.safeParseAsync(clientRequestBody)
+
+  if (!requestBody.success) {
+    return NextResponse.json({
+      error: true,
+      success: false,
+      message: 'Invalid request input',
+      errors: requestBody.error.errors
+    })
+  }
+
+  const { data } = requestBody
+
+  const updatedUser = await prisma.user.update({
+    where: {
+      id: user.id
+    },
+
+    data: {
+      favorites: {
+        createMany: {
+          data: data.products.map(product => ({
+            productId: product.id
+          })),
+
+          skipDuplicates: true
+        }
+      }
+    },
+
+    include: {
+      favorites: {
+        include: {
+          product: {
+            include: productIncludeFactory()
+          }
+        }
+      }
+    }
+  })
+
+  return NextResponse.json(updatedUser.favorites, {
+    headers: {
+      'x-favorites-count': String(updatedUser.favorites.length)
     }
   })
 }
