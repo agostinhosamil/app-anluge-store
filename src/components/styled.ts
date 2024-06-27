bf7fa45c43b64c46e42571535dc694e5f986472a
Agostinho Sam'l <agostinhosaml832@gmail.com>
2024-06-27 11:12:54 +0100

    feat(@components/styled): create Placeholder styled component based on animatedBackground css object

--- src/components/styled.ts
+++ src/components/styled.ts
@@ -1,4 +1,31 @@
-import styled from 'styled-components'
+import styled, { css, keyframes } from 'styled-components'
+
+const placeHolderShimmer = keyframes`
+  0%{
+    background-position: -450px 0
+  }
+
+  100%{
+      background-position: 450px 0
+  }
+`
+
+export const animatedBackground = css`
+  animation-duration: 1s;
+  animation-fill-mode: forwards;
+  animation-iteration-count: infinite;
+  animation-name: ${placeHolderShimmer};
+  animation-timing-function: linear;
+  background-color: #f6f7f8;
+  background-image: linear-gradient(
+    to right,
+    #f5f4f3 8%,
+    #ebebeb 18%,
+    #f5f4f3 33%
+  );
+  background-size: 1200px 104px;
+  position: relative;
+`
 
 export const LoadingScreen = styled.div`
   width: 100%;
@@ -67,4 +94,8 @@ export const UnderConstructionFallbackContainer = styled.div`
   @media (max-width: 920px) {
     padding: 30px;
   }
+`
+
+export const PlaceHolder = styled.div`
+  ${animatedBackground}
 `
