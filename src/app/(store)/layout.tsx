21f27e715e49635afdd4f17b35fe67e0b45c19fe
Agostinho Sam'l <agostinhosaml832@gmail.com>
2024-06-27 14:33:41 +0100

    feat(store@layout): [AssistantFloatingButton] render assistant chat floating button on store pages

--- src/app/(store)/layout.tsx
+++ src/app/(store)/layout.tsx
@@ -5,12 +5,14 @@ import { LayoutProps } from 'Types/next'
 import { Footer } from 'store@components/Footer'
 import { Header } from 'store@components/Header'
 import { Container } from 'store@components/styles'
+import { AssistantFloatingButton } from '~/components/store/AssistantFloatingButton'
 
 export default function StorePagesLayout({ children }: LayoutProps) {
   return (
     <Container>
       <Header />
       {children}
+      <AssistantFloatingButton />
       <Footer />
     </Container>
   )
