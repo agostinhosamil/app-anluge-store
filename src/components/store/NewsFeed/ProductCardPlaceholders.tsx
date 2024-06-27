d242a674be1004252828f11c001ad0b318d81cc6
Agostinho Sam'l <agostinhosaml832@gmail.com>
2024-06-27 11:17:27 +0100

    fix(store@components/NewsFeed/ProductCardPlaceholders): remove declarations for custom ProductCardPlaceholder props

--- src/components/store/NewsFeed/ProductCardPlaceholders.tsx
+++ src/components/store/NewsFeed/ProductCardPlaceholders.tsx
@@ -2,11 +2,4 @@ import { range } from '~/utils'
 import { ProductCardPlaceHolder } from './ProductCardPlaceHolder'
 
 export const ProductCardPlaceholders = () =>
-  range(30).map(i => (
-    <ProductCardPlaceHolder
-      key={i}
-      name={`Product ${i}`}
-      image="/image004.png"
-      description={`.`}
-    />
-  ))
+  range(30).map(i => <ProductCardPlaceHolder key={i} />)
