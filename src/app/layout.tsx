902acae38d9fd7daa48a122d7a110cfb7e5eeac3
Agostinho Sam'l <agostinhosaml832@gmail.com>
2024-06-28 16:30:19 +0100

    fix(root@layout): [og] correct logo image path

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -43,7 +43,7 @@ export const generateMetadata = (): Metadata => {
       url: origin,
       locale: 'pt-PT',
       phoneNumbers: companyData.phoneNumbers,
-      images: [`/anluge-logo.png`]
+      images: [`/anluge-logo.jpg`]
     }
   }
 }
