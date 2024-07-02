b713f5a39f0673fc3c58e0777d35b88fb7f67268
Agostinho Sam'l <agostinhosaml832@gmail.com>
2024-07-02 16:35:15 +0100

    fix: work on Image component

--- src/components/Image/index.tsx
+++ src/components/Image/index.tsx
@@ -2,21 +2,24 @@
 
 import { StaticImport } from 'next/dist/shared/lib/get-img-props'
 import NextImage, { ImageProps } from 'next/image'
-import { useState } from 'react'
+import { Fragment, useState } from 'react'
 
 import { noEmpty } from '~/utils'
 import errorImage from './error.png'
+import loadingImage from './loading.png'
+import { LoadingContainer } from './styles'
 
 type ImageComponent = React.FunctionComponent<ImageProps>
 
 export const Image: ImageComponent = props => {
   const [loading, setLoading] = useState<boolean>(false)
   const [error, setError] = useState<boolean>(false)
-  const [src, setSrc] = useState<string | StaticImport>(props.src)
+  const [src, setSrc] = useState<string | StaticImport>()
 
   const imageFetcher = async (): Promise<void> => {
     if (noEmpty(props.src)) {
       try {
+        console.log(`\n\n\n\n>>> Fetch Image: (${props.src})\n\n\n\n`)
         const response = await fetch(props.src)
         const imageData = await response.blob()
 
@@ -40,12 +43,20 @@ export const Image: ImageComponent = props => {
     imageFetcher()
   }
 
-  return loading ? null : (
-    <NextImage
-      {...props}
-      src={!error ? src : errorImage}
-      onError={nextImageErrorHandler}
-    />
+  const NextImageWrapper = loading ? LoadingContainer : Fragment
+
+  return (
+    <NextImageWrapper>
+      {noEmpty(src) ? (
+        <img {...props} src={src} />
+      ) : (
+        <NextImage
+          {...props}
+          src={!error ? (loading ? loadingImage : props.src) : errorImage}
+          onError={nextImageErrorHandler}
+        />
+      )}
+    </NextImageWrapper>
   )
 }
 
---
+++ src/components/Image/styles.ts
@@ -0,0 +1,9 @@
+import styled from 'styled-components'
+import { PlaceHolder } from '../styled'
+
+export const LoadingContainer = styled(PlaceHolder)`
+  width: 100%;
+  height: auto;
+  border-radius: inherit;
+  -webkit-border-radius: inherit;
+`
