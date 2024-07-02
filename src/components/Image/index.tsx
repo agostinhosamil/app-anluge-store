3b407e556f9d4d1beffe0e9a9df368c0bdac9269
Agostinho Sam'l <agostinhosaml832@gmail.com>
2024-07-02 16:21:32 +0100

    fix: work on Image component

--- src/components/Image/index.tsx
+++ src/components/Image/index.tsx
@@ -1,23 +1,49 @@
 'use client'
 
+import { StaticImport } from 'next/dist/shared/lib/get-img-props'
 import NextImage, { ImageProps } from 'next/image'
 import { useState } from 'react'
 
+import { noEmpty } from '~/utils'
 import errorImage from './error.png'
 
 type ImageComponent = React.FunctionComponent<ImageProps>
 
 export const Image: ImageComponent = props => {
+  const [loading, setLoading] = useState<boolean>(false)
   const [error, setError] = useState<boolean>(false)
+  const [src, setSrc] = useState<string | StaticImport>(props.src)
 
-  const nextImageErrorHandler = () => {
+  const imageFetcher = async (): Promise<void> => {
+    if (noEmpty(props.src)) {
+      try {
+        const response = await fetch(props.src)
+        const imageData = await response.blob()
+
+        if (imageData) {
+          setSrc(URL.createObjectURL(imageData))
+          setError(false)
+
+          return
+        }
+      } catch (err) {
+        // pass...
+      }
+    }
+
     setError(true)
   }
 
-  return (
+  const nextImageErrorHandler = () => {
+    setLoading(true)
+
+    imageFetcher()
+  }
+
+  return loading ? null : (
     <NextImage
       {...props}
-      src={!error ? props.src : errorImage}
+      src={!error ? src : errorImage}
       onError={nextImageErrorHandler}
     />
   )
