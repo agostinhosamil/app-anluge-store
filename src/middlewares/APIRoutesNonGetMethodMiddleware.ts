50fd8439d828a68a3e185bffce47f3be7beee105
Agostinho Sam'l <agostinhosaml832@gmail.com>
2024-07-06 12:32:54 +0100

    feat(app@middlewares): [APIRoutesNonGetMethodMiddleware] disable middleware

--- src/middlewares/APIRoutesNonGetMethodMiddleware.ts
+++ src/middlewares/APIRoutesNonGetMethodMiddleware.ts
@@ -1,8 +1,31 @@
 import { AppMiddleware } from 'Types/AppMiddleware'
 
+// import { auth } from '~/utils/auth'
+
 export const APIRoutesNonGetMethodMiddleware: AppMiddleware = {
   match: /^(\/api\/(.+))/i,
-  handler: ({ request, match }) => {
-    // console.log('Method -> ', request.method, '\nMatch => ', match)
+  handler: async () => {
+    // console.log(
+    //   '[APIRoutesNonGetMethodMiddleware] Method -> ',
+    //   request.method,
+    //   '\nMatch => ',
+    //   match
+    // )
+    // const requestMethod = request.method
+    // const publicMethodsRe = /^(get)$/i
+    // if (publicMethodsRe.test(requestMethod)) {
+    //   return
+    // }
+    // const signInData = await auth()
+    // if (!signInData) {
+    //   return response.json(
+    //     {
+    //       error: 'Unauthenticated'
+    //     },
+    //     {
+    //       status: 401
+    //     }
+    //   )
+    // }
   }
 }
