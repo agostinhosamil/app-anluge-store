92a9d1905874c2d94ac8f1c9ad0dea803c7622a8
Agostinho Sam'l <agostinhosaml832@gmail.com>
2024-07-07 15:23:57 +0100

    fix(store@components/NewsFeed/AdvertisingPanel): [styles] [Container > img] remove height style config

--- src/components/store/NewsFeed/AdvertisingPanel/styles.ts
+++ src/components/store/NewsFeed/AdvertisingPanel/styles.ts
@@ -19,23 +19,10 @@ export const Container = styled.article`
 
       img {
         width: 100%;
-        max-height: 420px;
         object-fit: cover;
         -webkit-object-fit: cover;
         border-radius: inherit;
         -webkit-border-radius: inherit;
-
-        @media (max-width: 920px) {
-          max-height: 220px;
-        }
-
-        @media (min-width: 1300px) {
-          max-height: 560px;
-        }
-
-        @media (min-width: 1700px) {
-          max-height: 680px;
-        }
       }
     }
 
