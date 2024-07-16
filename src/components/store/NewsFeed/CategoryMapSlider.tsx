8be0bf157809f4ae1bef0bebed955475cce580f7
Agostinho Sam'l <agostinhosaml832@gmail.com>
2024-07-16 12:18:17 +0100

    feat(store@components/NewsFeed/CategoryMapSlider): create category map slider server componnet

---
+++ src/components/store/NewsFeed/CategoryMapSlider.tsx
@@ -0,0 +1,70 @@
+import Link from 'next/link'
+import { Fragment } from 'react'
+
+import { PlaceHolder } from '@components/PlaceHolder'
+import { prisma } from '@services/prisma'
+import { Marquee } from '~/components/Marquee'
+import { arraySplit } from '~/utils'
+
+type CategoryMapSliderProps = {
+  rows?: number | `${number}`
+}
+
+export const CategoryMapSlider = async (props: CategoryMapSliderProps) => {
+  const categories = await prisma.category.findMany({
+    select: {
+      slag: true,
+      icon: true,
+      title: true
+    }
+  })
+
+  if (categories.length <= 0) {
+    return null
+  }
+
+  const isOdd = (num: number) => Boolean(num / 2 !== parseInt(String(num / 2)))
+
+  const categoriesRowsCount =
+    props.rows && !isNaN(Number(props.rows)) ? Number(props.rows) : 2
+  const categoriesRows = arraySplit(
+    categories,
+    Math.ceil(categories.length / categoriesRowsCount)
+  )
+
+  // console.log(categoriesRows)
+
+  return (
+    <Fragment>
+      <h1 className="text-pretty px-[20px] font-light uppercase mb-[50px]">
+        Comprar por categorias
+      </h1>
+      <div className="w-full h-max mb-[calc(6*24px)] mt-[calc(4*24px)] flex flex-col gap-3 relative">
+        <Marquee>
+          {categoriesRows.map((categoryRow, categoryRowIndex) => (
+            <div
+              key={categoryRowIndex}
+              className={`w-full flex flex-row gap-[150px] flex-nowrap`.concat(
+                isOdd(categoryRowIndex) ? ' pl-[150px]' : ''
+              )}
+            >
+              {categoryRow.map(category => (
+                <Link
+                  key={category.slag}
+                  href={`/categories/${category.slag}?ref=categories.slider@map`}
+                  className="w-[150px] h-max block relative"
+                >
+                  <PlaceHolder className="bg-zinc-200 rounded-full size-[150px]">
+                    <span className="w-[150px] break-words whitespace-break-spaces px-2 line-clamp-6 absolute top-full mt-[+20px] text-center font-light text-zinc-900 select-none pointer-events-none">
+                      {category.title}
+                    </span>
+                  </PlaceHolder>
+                </Link>
+              ))}
+            </div>
+          ))}
+        </Marquee>
+      </div>
+    </Fragment>
+  )
+}
