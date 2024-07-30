9a0706422bd32c9fa9e6bc690f3ba4291ec4ad4e
Agostinho Sam'l <agostinhosaml832@gmail.com>
2024-07-30 14:14:28 +0100

    feat(@utils/category/imageUploadClient): [types] create uplaoded image sizes types for both icon and banner

--- src/utils/category/imageUploadClient.ts
+++ src/utils/category/imageUploadClient.ts
@@ -1,25 +1,34 @@
 import { AnlugeUploadClient } from '@services/upload'
 
+export const bannerUploadedImageSizes = {
+  'x-small': '339x181',
+  small: '510x272',
+  normal: '500x500',
+  medium: '880x470',
+  large: '1500x800'
+}
+
+export const iconUploadedImageSizes = {
+  'x-small': '339x339',
+  small: '410x410',
+  normal: '500x500',
+  medium: '880x880',
+  large: '1200x1200'
+}
+
+export type IconUploadedImageSizes = typeof iconUploadedImageSizes
+export type IconUploadedImageSize = keyof IconUploadedImageSizes
+export type BannerUploadedImageSizes = typeof bannerUploadedImageSizes
+export type BannerUploadedImageSize = keyof BannerUploadedImageSizes
+
 export const categoryBannerUploadClient = new AnlugeUploadClient({
   imageSet: 'categories',
   uploadedImageObjectFit: 'fill',
-  uploadedImageSizes: {
-    'x-small': '339x181',
-    small: '510x272',
-    normal: '500x500',
-    medium: '880x470',
-    large: '1500x800'
-  }
+  uploadedImageSizes: bannerUploadedImageSizes
 })
 
 export const categoryIconUploadClient = new AnlugeUploadClient({
   imageSet: 'categories',
   uploadedImageObjectFit: 'fill',
-  uploadedImageSizes: {
-    'x-small': '339x339',
-    small: '410x410',
-    normal: '500x500',
-    medium: '880x880',
-    large: '1200x1200'
-  }
+  uploadedImageSizes: iconUploadedImageSizes
 })
