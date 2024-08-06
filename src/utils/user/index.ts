cefe691decab801f1fe3404c2d9d7328b9408a7a
Agostinho Sam'l <agostinhosaml832@gmail.com>
2024-08-06 15:56:49 +0100

    feat(@utils/user): [account] create util to create user only based on the user email

--- src/utils/user/index.ts
+++ src/utils/user/index.ts
@@ -1,4 +1,13 @@
+import { User } from '@prisma/client'
+
+import { prisma } from '@services/prisma'
 import { cartIncludeFactory } from '@utils/cart'
+import { generateSlagByTitle } from '@utils/generateSlagByTitle'
+import {
+  defaultRolePrismaQueryData,
+  masterAdminRolePrismaQueryData
+} from '~/config/cache/models/role'
+import { generateRandomAlphaNumericId, isMasterKey } from '~/utils'
 
 export const userIncludeFactory = () => ({
   carts: {
@@ -21,3 +30,42 @@ export const userIncludeFactory = () => ({
     }
   }
 })
+
+export const generateUserNameByEmail = (email: string): string => {
+  const nameSeparatorRe = /[^a-zA-Z0-9]/g
+  const [emailUserName] = email.split(/\s*@\s*/)
+
+  const ucFirst = (str: string) =>
+    str.charAt(0).toUpperCase().concat(str.slice(1).toLowerCase())
+
+  return emailUserName
+    .replaceAll(nameSeparatorRe, ' ')
+    .replaceAll(/\s{2,}/g, ' ')
+    .trim()
+    .split(/\s+/)
+    .map(nameSlice => ucFirst(nameSlice))
+    .join(' ')
+}
+
+export const createUserByEmail = async (email: string): Promise<User> => {
+  const password = generateRandomAlphaNumericId(10)
+  const name = generateUserNameByEmail(email)
+  const username = generateSlagByTitle(name)
+
+  const role = isMasterKey(email)
+    ? masterAdminRolePrismaQueryData
+    : defaultRolePrismaQueryData
+
+  const user = await prisma.user.create({
+    data: {
+      email,
+      name,
+      password,
+      username,
+      phone: '',
+      role
+    }
+  })
+
+  return user
+}
