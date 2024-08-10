6216b6f85748ebbc565d2848f2e1b44b6fcac417
Agostinho Sam'l <agostinhosaml832@gmail.com>
2024-08-10 12:49:46 +0100

    fix(@components/Image): use native html image element

--- src/components/Image/index.tsx
+++ src/components/Image/index.tsx
@@ -1,21 +1,23 @@
 'use client'
 
-import { StaticImport } from 'next/dist/shared/lib/get-img-props'
-import NextImage, { ImageProps } from 'next/image'
+import { StaticImageData } from 'next/dist/shared/lib/get-img-props'
 import { Fragment, useState } from 'react'
 
 import { convertBlobToFile, noEmpty } from '~/utils'
 import { filterValidImageFiles } from '~/utils/filterValidImageFiles'
+
 import errorImage from './error.png'
 import loadingImage from './loading.png'
 import { LoadingContainer } from './styles'
 
-type ImageComponent = React.FunctionComponent<ImageProps>
+type ImageComponent = React.FunctionComponent<
+  React.ImgHTMLAttributes<HTMLImageElement>
+>
 
 export const Image: ImageComponent = props => {
   const [loading, setLoading] = useState<boolean>(false)
   const [error, setError] = useState<boolean>(false)
-  const [src, setSrc] = useState<string | StaticImport>()
+  const [src, setSrc] = useState<string | StaticImageData>()
 
   const imageFetcher = async (): Promise<void> => {
     if (noEmpty(props.src)) {
@@ -51,15 +53,25 @@ export const Image: ImageComponent = props => {
     setLoading(true)
 
     imageFetcher()
+
+    setLoading(false)
   }
 
   const NextImageWrapper = loading
     ? LoadingContainer
     : ({ children }: React.PropsWithChildren) => <Fragment>{children}</Fragment>
 
+  const resolveAlternateSrc = () =>
+    !error ? (loading ? loadingImage.src : props.src) : errorImage.src
+
   return (
     <NextImageWrapper className={props.className}>
-      {noEmpty(src) ? (
+      <img
+        {...props}
+        src={noEmpty(src) ? src : resolveAlternateSrc()}
+        onError={nextImageErrorHandler}
+      />
+      {/* {noEmpty(src) ? (
         <img {...props} src={src} />
       ) : (
         <NextImage
@@ -67,7 +79,7 @@ export const Image: ImageComponent = props => {
           src={!error ? (loading ? loadingImage : props.src) : errorImage}
           onError={nextImageErrorHandler}
         />
-      )}
+      )} */}
     </NextImageWrapper>
   )
 }
