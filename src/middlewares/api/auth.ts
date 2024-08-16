54cc467fe0d6f9a71ca29a5a4464488896edc227
Agostinho Sam'l <agostinhosaml832@gmail.com>
2024-08-16 19:57:22 +0100

    feat(api@middlewares/auth): [jwt] create jwt authentication middleware

--- src/middlewares/api/auth.ts
+++ src/middlewares/api/auth.ts
@@ -1,6 +1,26 @@
+import { auth as getSignInData } from '@utils/auth'
+import { MiddlewareActionProps } from 'Types/api'
+
 export const auth = {
-  jwt: async () => {
-    console.log('JWT middleware applied')
+  jwt: async ({ response, request }: MiddlewareActionProps) => {
+    const authorizationHeader = request.headers.get('Authorization')
+    const authToken = String(authorizationHeader).replace(/^(Bearer\s+)/i, '')
+    const signInData = await getSignInData({
+      token: authToken
+    })
+
+    if (!signInData) {
+      return response.json(
+        {
+          status: 'error',
+          error: 'unauthenticated',
+          message: 'not authenticated'
+        },
+        {
+          status: 401
+        }
+      )
+    }
   },
 
   vendor: {
