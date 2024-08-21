42368b13c0a9b04d1cd9e06acdf87962c8eba69b
Agostinho Sam'l <agostinhosaml832@gmail.com>
2024-08-21 16:04:50 +0100

    feat(@components/ApplicationContext): create application context util to open a dialog form

--- src/components/ApplicationContext/index.tsx
+++ src/components/ApplicationContext/index.tsx
@@ -1,5 +1,11 @@
 'use client'
 
+import { useRef, useState } from 'react'
+
+import { Dialog } from '@components/Dialog'
+
+import { formDataToJson } from '~/utils/formDataToJson'
+import { FormWrapper } from './components/FormWrapper'
 import { ApplicationContext } from './context'
 import { ApplicationContextProps } from './types'
 import { appOriginValueFallback } from './utils'
@@ -18,15 +24,107 @@ type ApplicationContextProviderComponent = React.FunctionComponent<
   React.PropsWithChildren & ApplicationContextProviderProps
 >
 
+type FormDialogSubmitHandler = (event: SubmitEvent) => void
+
 export const ApplicationContextProvider: ApplicationContextProviderComponent =
   props => {
+    const [showFormDialog, setShowFormDialog] = useState<boolean>(false)
+    const [FormDialogContent, setFormDialogContent] =
+      useState<React.ElementType>()
+
+    const formDialogSubmitHandlerState = useRef<FormDialogSubmitHandler>()
+    const formDialogCloseHandlerState = useRef<any>()
+
+    const reopenLastFormDialog = () => {
+      setShowFormDialog(true)
+    }
+
+    const closeFormDialog = () => {
+      setShowFormDialog(false)
+    }
+
     const applicationContextData: ApplicationContextProps = {
-      origin: props.headers['x-app-origin'] || appOriginValueFallback()
+      origin: props.headers['x-app-origin'] || appOriginValueFallback(),
+
+      openFormDialog: (formDataObjectSchema, formElement) => {
+        // const formDataObjectSchema =
+        //   args[0] instanceof z.ZodObject ? args[0] : null
+        // const formElement = args[-1 + args.length]
+
+        if (typeof formElement !== 'function') {
+          throw new Error('form element must be a valid react component')
+        }
+
+        setShowFormDialog(true)
+
+        // console.log('formElement => ', formElement)
+
+        setFormDialogContent(() => formElement)
+
+        return new Promise(resolve => {
+          const defaultResponseData = {
+            reopen: reopenLastFormDialog,
+            close: closeFormDialog
+          }
+
+          formDialogCloseHandlerState.current = () => {
+            resolve({
+              ...defaultResponseData,
+              error: true,
+              success: false,
+              data: null
+            })
+          }
+
+          formDialogSubmitHandlerState.current = async event => {
+            const formElement = event.target as HTMLFormElement
+            const formData = new FormData(formElement)
+            const jsonFormData = formDataToJson(formData)
+
+            const validatedFormData =
+              await formDataObjectSchema.safeParseAsync(jsonFormData)
+
+            if (validatedFormData.success) {
+              setShowFormDialog(false)
+              // setFormDialogContent(undefined)
+              resolve({
+                ...defaultResponseData,
+                error: false,
+                success: true,
+                data: validatedFormData.data
+              })
+            }
+          }
+        })
+      }
+    }
+
+    const formDialogCloseHandler = () => {
+      setShowFormDialog(false)
+
+      if (typeof formDialogCloseHandlerState.current === 'function') {
+        formDialogCloseHandlerState.current()
+      }
+    }
+
+    const formDialogSubmitHandler = (event: SubmitEvent) => {
+      if (typeof formDialogSubmitHandlerState.current === 'function') {
+        formDialogSubmitHandlerState.current(event)
+      }
     }
 
     return (
       <ApplicationContext.Provider value={applicationContextData}>
         {props.children}
+        <Dialog
+          size="x-large"
+          show={showFormDialog}
+          onClose={formDialogCloseHandler}
+        >
+          <FormWrapper onSubmit={formDialogSubmitHandler}>
+            {FormDialogContent && <FormDialogContent />}
+          </FormWrapper>
+        </Dialog>
       </ApplicationContext.Provider>
     )
   }
--- src/components/ApplicationContext/types.ts
+++ src/components/ApplicationContext/types.ts
@@ -1,3 +1,39 @@
+import { z } from 'zod'
+
+export type OpenFormDialogUtilArgsWithDataSchema<
+  FormDataObjectType extends z.ZodRawShape
+> = [z.ZodObject<FormDataObjectType, 'strip', z.ZodTypeAny>, React.ElementType]
+export type OpenFormDialogUtilArgsWithoutDataSchema = [React.ElementType]
+
+export type OpenFormDialogUtilDefaultResponse = {
+  reopen: () => void
+  close: () => void
+}
+
+export type OpenFormDialogUtilFailureResponse =
+  OpenFormDialogUtilDefaultResponse & {
+    error: true
+    success: false
+    data: null
+  }
+
+export type OpenFormDialogUtilSuccessResponse<Data> =
+  OpenFormDialogUtilDefaultResponse & {
+    error: false
+    success: true
+    data: Data
+  }
+
 export type ApplicationContextProps = {
   origin: string
+
+  openFormDialog: <FormDataObjectType extends z.ZodRawShape>(
+    ...[
+      formDataSchema,
+      formElement
+    ]: OpenFormDialogUtilArgsWithDataSchema<FormDataObjectType>
+  ) => Promise<
+    | OpenFormDialogUtilFailureResponse
+    | OpenFormDialogUtilSuccessResponse<z.infer<typeof formDataSchema>>
+  >
 }
