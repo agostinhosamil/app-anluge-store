b8abc3d2650e4eb26a81ed22ab7777c1763ad526
Agostinho Sam'l <agostinhosaml832@gmail.com>
2024-09-10 15:52:20 +0100

    feat(dashboard@pages/highlights/components/CategoriesList): add remove highlight button functionality

--- src/app/(dashboard)/dashboard/highlights/components/CategoriesList.tsx
+++ src/app/(dashboard)/dashboard/highlights/components/CategoriesList.tsx
@@ -6,9 +6,14 @@ import Image from '@components/Image'
 import { uploadedImageUrl } from '~/utils'
 
 import { useHighlights } from '../hook'
+import { HighlightCategoryInput } from '../types'
 
 export const CategoriesList: React.FunctionComponent = () => {
-  const { highlights } = useHighlights()
+  const { highlights, removeHighlight } = useHighlights()
+
+  const removeButtonClickHandler = (highlight: HighlightCategoryInput) => {
+    removeHighlight(highlight)
+  }
 
   return (
     <ul className="w-full flex flex-row flex-wrap">
@@ -31,7 +36,14 @@ export const CategoriesList: React.FunctionComponent = () => {
                   {category.title}
                 </strong>
                 <div className="w-full flex justify-center flex-row gap-3">
-                  <button className="rounded-lg border-0 outline-none bg-red-400 hover:bg-red-500 active:bg-red-600 text-zinc-50 dark:bg-red-600 dark:hover:bg-red-700 dark:active:bg-red-800 py-2 px-3 flex flex-row gap-2 items-center">
+                  <button
+                    className="rounded-lg border-0 outline-none bg-red-400 hover:bg-red-500 active:bg-red-600 text-zinc-50 dark:bg-red-600 dark:hover:bg-red-700 dark:active:bg-red-800 py-2 px-3 flex flex-row gap-2 items-center"
+                    onClick={() =>
+                      removeButtonClickHandler({
+                        category: category.id
+                      })
+                    }
+                  >
                     <FaX />
                     <span className="text-sm">Remover dos destaques</span>
                   </button>
