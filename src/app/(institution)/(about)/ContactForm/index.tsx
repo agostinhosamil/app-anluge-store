d3cdbb710df7896c4b7c9820052b6fdf7e85f36b
Agostinho Sam'l <agostinhosaml832@gmail.com>
2024-09-11 16:59:53 +0100

    feat(institution@pages): [ContactForm] change map address

--- src/app/(institution)/(about)/ContactForm/index.tsx
+++ src/app/(institution)/(about)/ContactForm/index.tsx
@@ -11,13 +11,23 @@ export const ContactForm = () => {
         id="map"
         className="relative h-[300px] overflow-hidden rounded-xl bg-cover bg-[50%] bg-no-repeat"
       >
-        <iframe
+        {/* <iframe
           src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d11672.945750644447!2d13.2189448!3d-8.8158034!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x0!2sLocation!5e0!3m2!1sen!2sus!4v1619524992238!5m2!1sen!2sus"
           width="100%"
           height="680"
           className="border-0 outline-none rounded-xl bg-blue-500"
           allowFullScreen
           loading="lazy"
+        /> */}
+
+        <iframe
+          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d353.58247914049633!2d13.240140230319524!3d-8.817110669870955!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x1a51f24bb4064e8d%3A0xee83582c921a550e!2sFarm%C3%A1cia%20Mecofarma%20-%20Kinaxixi!5e1!3m2!1sen!2sao!4v1726045354563!5m2!1sen!2sao"
+          width="100%"
+          height="450"
+          className="border-0 outline-none rounded-xl bg-blue-500"
+          allowFullScreen
+          loading="lazy"
+          referrerPolicy="no-referrer-when-downgrade"
         />
       </div>
       <div className="container px-6 md:px-12">
